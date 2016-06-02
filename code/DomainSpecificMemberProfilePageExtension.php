d9a19984abdd5c229532e8c77fb132e7dedb2222
Maxime Rainville <max@firebrand.co.nz>
2016-06-02 14:21:54 +1200

    Replicate the logic from MemberProfilePage to create CheckableVisibilityField

--- code/DomainSpecificMemberProfilePageExtension.php
+++ code/DomainSpecificMemberProfilePageExtension.php
@@ -1,4 +1,5 @@
 <?php
+use MemberProfileField;
 
 class DomainSpecificMemberProfilePageExtension extends Extension
 {
@@ -6,21 +7,38 @@ class DomainSpecificMemberProfilePageExtension extends Extension
     public function updateProfileFields(FieldList $fields)
     {
         $owner = $this->getOwner();
-        $fieldDO = $owner->Fields()->where(['MemberField' => 'Email'])->First();
 
-
+        $profileField = $owner->Fields()->where(['MemberField' => 'Email'])->First();
         $oldField = $fields->fieldByName('Email');
-        if (is_a($oldField, 'CheckableVisibilityField')) {
 
-        } else {
-            $newField = DomainSpecificEmailField::createFrom($oldField);
-            $newField
-                ->setAllowedDomains($fieldDO->AllowedDomains)
-                ->setDisallowedDomains($fieldDO->DisallowedDomains)
-                ->setShowListOnError($fieldDO->ShowDomainsOnError);
-
-            $fields->replaceField('Email', $newField);
+        // If we are missing the field or $profileField DataObject for Email, do nothing.
+        if (!$profileField || !$profileField->ID || !$oldField) {
+            return;
         }
+
+        if (is_a($oldField, 'CheckableVisibilityField')) {
+            $field = $this->initNewField($oldField->getChild(), $profileField);
+            $field = new CheckableVisibilityField($field);
+
+            if ($profileField->PublicVisibility == 'Display') {
+                $field->makeAlwaysVisible();
+            } else {
+                $field->getCheckbox()->setValue($profileField->PublicVisibilityDefault);
+            }
+        } else {
+            $field = $this->initNewField($oldField, $profileField);
+        }
+        $fields->replaceField('Email', $field);
+    }
+
+    protected function initNewField(TextField $oldField, MemberProfileField $profileField)
+    {
+        $field = DomainSpecificEmailField::createFrom($oldField);
+        $field
+            ->setAllowedDomains($profileField->AllowedDomains)
+            ->setDisallowedDomains($profileField->DisallowedDomains)
+            ->setShowListOnError($profileField->ShowDomainsOnError);
+        return $field;
     }
 
 }
